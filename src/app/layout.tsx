ede6416184a741e3c535f7908179a354842738bb
macminhtung <macminhtung1993@gmail.com>
2025-07-11 14:44:49 +0700

    update README file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Geist } from 'next/font/google';
+
+const geist = Geist({
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-  title: 'Create Next App',
+  title: '[MMT] NextJs',
 };
 
 export default function RootLayout({
@@ -11,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={geist.className}>
       <body>{children}</body>
     </html>
   );
